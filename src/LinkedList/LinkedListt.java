1e8eba5056ea38cfc47ee98f5c64d9602a5d0f0c
Madhusudan <ms9338504@gmail.com>
2024-07-06 23:07:47 +0530

    LinkedList Methods

--- src/LinkedList/LinkedListt.java
+++ src/LinkedList/LinkedListt.java
@@ -4,8 +4,8 @@ public class LinkedListt {
     public static void main(String[] args) {
         int[] arr={1,22,34,5,6,7};
         Node head=convertArrToLL(arr);
-        print(deleteHead(head));
 
+        print(head);
 
 
     }
@@ -54,6 +54,73 @@ public class LinkedListt {
         return head;
 
     }
+    private static Node deleteKthPosition(Node head,int k){
+        if(head == null ) return null;
+        Node temp =head;
+        if (k==1){
+            head=head.next;
+            return head;
+        }
+        int count=0;
+        Node previous= null;
+        while (temp != null){
+            count++;
+            if (count==k){
+                previous.next=previous.next.next;
+                break;
+            }
+            previous=temp;
+            temp=temp.next;
+
+        }
+        return head;
+    }
+    private static Node deleteTail(Node head){
+        if (head == null||head.next==null) return head;
+        Node temp=head;
+        while (temp.next.next != null){
+             temp=temp.next;
+        }
+        temp.next=null;
+        return head;
+    }
+    private static Node deleteElement(Node head,int value){
+        if (head==null) return null;
+
+        if (head.data==value){
+            head=head.next;
+            return head;
+        }
+        Node previous=null;
+        Node temp=head;
+        while (temp != null){
+            if (temp.data==value){
+                previous.next=previous.next.next;
+                break;
+            }
+            previous=temp;
+            temp=temp.next;
+
+        }
+        return head;
+    }
+    private static Node insertValueAtFirst(Node head,int value){
+
+        return new Node(value,head);
+    }
+    private static Node insertValueAtLast(Node head,int value){
+        if(head==null){
+            return new Node(value);
+        }
+        Node temp=head;
+        while (temp.next !=null){
+            temp=temp.next;
+            }
+            temp.next=new Node(value);
+
+
+    return head;
+    }
 
 
 
@@ -66,6 +133,11 @@ class Node{
         this.data = data;
         this.next=null;
     }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 }
 /*
         System.out.println("head of LL->"+head.data);
@@ -76,4 +148,9 @@ class Node{
         }System.out.println(isPresent(head,7));
         head=deleteHead(head);
         System.out.println("head of LL->"+head.data);
+         Node head=convertArrToLL(arr);
+        print(deleteHead(head));
+        head=deleteKthPosition(head,5);
+        head=deleteElement(head,34);
+         head=insertValueAtLast(head,12);
  */

ec1982ab316dd5909338d1ecbfdb19454105e9f5
Madhusudan <ms9338504@gmail.com>
2024-07-08 11:46:47 +0530

    Double LinkedList Methods

--- src/LinkedList/DoublyLinkedList.java
+++ src/LinkedList/DoublyLinkedList.java
@@ -1,12 +1,103 @@
 package LinkedList;
 
+import ArrayQuestion.NextPermutation;
+
 public class DoublyLinkedList {
     public static void main(String[] args) {
         int[] arr={1,3,4,2,6};
         NodeForDLL head=convertArrayToDll(arr);
-        head=deleteLast(head);
+//        deleteNode(head.next.next);
+        head=insertValAtK(head,3,23);
         print(head);
     }
+
+    private static NodeForDLL insertValAtK(NodeForDLL head,int k,int val){
+        if (k==1) return insertBeforeHead(head,val);
+
+        NodeForDLL temp=head;
+        int count=0;
+        while (temp != null){
+            count++;
+            if (count==k) break;
+            temp=temp.next;
+        }
+        NodeForDLL previous=temp.back;
+        NodeForDLL newNode=new NodeForDLL(val,temp,previous);
+        previous.next=newNode;
+        temp.back=newNode;
+
+        return head;
+
+    }
+    private static NodeForDLL insertBeforeTail(NodeForDLL head,int value){
+        if (head.next==null){
+            return insertBeforeHead(head,value);
+        }
+        NodeForDLL tail=head;
+        while (tail.next != null){
+            tail=tail.next;
+        }
+        NodeForDLL previous=tail.back;
+        NodeForDLL newNode=new NodeForDLL(value,tail,previous);
+        previous.next=newNode;
+        tail.back=newNode;
+        return head;
+
+    }
+
+    private static NodeForDLL insertBeforeHead(NodeForDLL head,int value){
+        NodeForDLL newHead=new NodeForDLL(value,head,null);
+
+        head.back=newHead;
+        return newHead;
+    }
+
+    private static void deleteNode(NodeForDLL temp){
+        NodeForDLL prev=temp.back;
+        NodeForDLL front=temp.next;
+        if (front==null){
+            prev.next=null;
+            temp.back=null;
+        }
+        prev.next=front;
+        front.back=prev;
+        temp.next=temp.back=null;
+
+    }
+
+    private static NodeForDLL deleteK(NodeForDLL head,int k){
+        if (head==null) return null;
+
+        NodeForDLL temp=head;
+
+        int count=0;
+
+        while (temp != null){
+            count++;
+            if (count==k) {break;}
+            temp=temp.next;
+        }
+
+        NodeForDLL prev=temp.back;
+        NodeForDLL front=temp.next;
+
+
+        if (prev ==null && front==null){
+            return null;
+        }
+        else if (prev==null){
+            return deleteFirst(head);
+        } else if (front==null) {
+           return deleteLast(head);
+        }
+            prev.next=front;
+            front.back=prev;
+            temp.next=null;
+            temp.back=null;
+
+        return head;
+    }
+
     private static NodeForDLL deleteLast(NodeForDLL head){
         if (head==null || head.next==null) return null;
         NodeForDLL tail=head;
@@ -17,7 +108,6 @@ public class DoublyLinkedList {
         NodeForDLL prev=tail.back;
         prev.next=null;
         tail.back=null;
-
         return head;
     }
     private static NodeForDLL deleteFirst(NodeForDLL head){
